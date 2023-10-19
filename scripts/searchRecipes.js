d17415916c34565e239916afecf39b61922368f2
Ahearn <ahearn1@emich.edu>
2023-10-19 12:28:13 -0400

    Feat: Search term on enter
    *takes input from search
    *creates and displays list of recipes

--- scripts/searchRecipes.js
+++ scripts/searchRecipes.js
@@ -1,53 +1,66 @@
-/**
- * 1st function to retrieve content from json
- * 2nd function search nested keys in json
- * another function to display them
- */
-
-
-
 //test term
-const term = "egg yolk";
+//const term = 'egg yolk';
 
+const recipes = []; // array to hold recipes
+
+//enter key
+const element = document.getElementById('term');
+element.addEventListener('keydown', function (button) {
+	if (button.key === 'Enter') {
+		recipes.length = 0; //reset array
+		display();
+		getJson(element.value);
+	}
+});
 
 // retrieve json data
-function getJson(){
-    fetch('recipes.json')
-    .then(res => res.json())
-    .then(data => {
-        //console.log(data);
-        console.log("get Json");
-        termSearch(data, term);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+function getJson(term) {
+	console.log(term);
+	fetch('assets/recipes.json')
+		.then((res) => res.json())
+		.then((data) => {
+			//console.log(data);
 
-const recipes = []; // list to hold recipe
+			termSearch(data, term);
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+}
 
 //searches array of components and adds them to list
 //calls display to output recipe names
-function termSearch(data, term){
-    for(let i = 0; i< data.results.length;i++){
+function termSearch(data, term) {
+	for (let i = 0; i < data.results.length; i++) {
+		for (let j = 0; j < data.results[i].sections.length; j++) {
+			for (
+				let k = 0;
+				k < data.results[i].sections[j].components.length;
+				k++
+			) {
+				if (
+					data.results[i].sections[j].components[
+						k
+					].ingredient.name.includes(term)
+				) {
+					recipes.push(data.results[i].name);
+				}
+			}
+		}
+	}
 
-        for(let j= 0; j < data.results[i].sections.length;j++){
-
-            for(let k = 0; k < data.results[i].sections[j].components.length;k++){
-
-                if(data.results[i].sections[j].components[k].ingredient.name.includes(term)){
-                    recipes.push(data.results[i].name);
-                }
-            }
-        }
-    }
-
-    display();
+	display();
 }
+
 //displays list
-function display(){
-    for (let i = 0; i < recipes.length; i++) {
-        console.log(recipes[i]); 
-      }
-}
+function display() {
+	let recipeList = document.getElementById('list');
 
+	recipeList.innerHTML = ''; //reset list
+
+	for (let i = 0; i < recipes.length; i++) {
+		let li = document.createElement('li');
+		li.innerText = recipes[i];
+		recipeList.appendChild(li);
+	}
+}
