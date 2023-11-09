10212e97b2cb3ef88e5bd01c5441b46c97f8f307
nereidarondon <nereida.rondon5@gmail.com>
2023-11-08 21:21:59 -0500

    feature: darkmode works individually on each page

--- scripts/searchRecipes.js
+++ scripts/searchRecipes.js
@@ -1,7 +1,7 @@
 //test term
 //const term = 'egg yolk';
 const body = document.getElementsByTagName('body')[0];
-const headers = document.getElementsByClassName('page-header');
+const header = document.getElementById('header');
 var recipes = []; // array to hold recipes
 let recipeList = document.getElementById('list');
 const element = document.getElementById('term');
@@ -11,20 +11,25 @@ let elementNoSpace;
 function theme(){
 
   console.log(body.getAttribute("data-bs-theme"));
-
-  if(body.getAttribute("data-bs-theme","dark")=== "dark"){
-    console.log("if", body.getAttribute("data-bs-theme"));
+  if(body.getAttribute("data-bs-theme","dark") === "dark"){
     body.setAttribute("data-bs-theme","light");
-    headers[0].classList.add("page-header");
-    headers[0].classList.remove("darkmode-h1");
-  }else{
-    console.log("else", body.getAttribute("data-bs-theme"));
-    body.setAttribute("data-bs-theme","dark");
-    headers[0].classList.add("darkmode-h1");
-    headers[0].classList.remove("page-header");
+    localStorage.setItem("theme-mode", "light");
 
-  }
+  }else{
+    body.setAttribute("data-bs-theme","dark");
+    localStorage.setItem("theme-mode", "dark");
+
+  }  
+
+  header.classList.toggle("darkmode-h1");
+  header.classList.toggle("page-header");
+  //header.classList.toggle("darkmode-h1");
+  
 }
+window.addEventListener('load', (event) => {
+  console.log('page is fully loaded');
+});
+
 
 //enter key
 element.addEventListener('keydown', function (button) {
@@ -101,7 +106,6 @@ function termSearch(data, term) {
 	display();
 }
 
-
 //displays list of recipes as cards
   function display(){
   if (recipes.length === 0) {
@@ -109,36 +113,26 @@ function termSearch(data, term) {
   }
   
 	let recipeCard = '';
-	
   for(let i = 0; i < recipes.length; i++){
     
     console.log(recipes[i].name);
 
     recipeCard += `
-    
-    <div class="recipe-card-div card h-100 m-2 p-0 text-black col-sm-3 shadow">
-    
-    <img src=${recipes[i].beauty_url ? recipes[i].beauty_url:recipes[i].thumbnail_url} class="card-img-top overflow-hidden ratio ratio-1x1" alt="" width="240" height="240" >
-    
-    <div class="card-body h-100">
-    
-    <h5 class="card-title recipe-card-title">${recipes[i].name}</h5>
-    <div class="d-flex flex-row justify-content-between">
-    <p class="card-text fw-bold">${(recipes[i].user_ratings.score*5).toFixed(1) + '⭐'}</p>
-    <p class="card-text fw-bold">${recipes[i].tags[0].display_name}</p>
-    </div>
-    <button class="btn btn-success" onclick="showModal(${i})">View Recipe</button>
-    
-          </div>
-          
-          </div>
-          `;
+        <div class="recipe-card-div card h-100 m-2 p-0 col-sm-3 shadow">
+          <img src=${recipes[i].beauty_url ? recipes[i].beauty_url:recipes[i].thumbnail_url} class="card-img-top overflow-hidden ratio ratio-1x1" alt="" width="240" height="240" >
+          <div class="card-body h-100">
+            <h5 class="card-title recipe-card-title">${recipes[i].name}</h5>
+            <div class="d-flex flex-row justify-content-between">
+              <p class="card-text fw-bold">${(recipes[i].user_ratings.score*5).toFixed(1) + '⭐'}</p>
+              <p class="card-text fw-bold">${recipes[i].tags[0].display_name}</p>
+            </div>
+            <button class="btn btn-success" onclick="showModal(${i})">View Recipe</button>
+          </div>      
+        </div>
+      `;
   }
-
   recipeList.innerHTML = recipeCard;
-  
 }
-
 const modal = document.createElement('div');
 
 // Access JSON data from array when the recipe button is clicked
@@ -166,8 +160,7 @@ function showModal(id){
   }
 
   //structure the html in the modal
-  modal.innerHTML = `
-    
+  modal.innerHTML = `   
       <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
           <div class="modal-header">
@@ -175,8 +168,7 @@ function showModal(id){
             <h1 class="modal-title fs-4 text-center">${recipes[id].name}</h1>
 
             <button type="button" class="btn-close" onclick="closeModal()" aria-label="Close"></button>
-          </div>
-          
+          </div>         
                 <div class="modal-body p-5">
           
                     <div class="container-fluid ">
@@ -263,86 +255,11 @@ function showModal(id){
         </div>
       </div>
     `;
-  
   document.body.appendChild(modal);  //add to DOM
-
   modal.style.display = 'block';  //show the modal
 }
-
 // close modal
 function closeModal() {
   //modal.style.display = 'none'; // hide the modal
   modal.remove(); // hide the modal
 }
-
-
-//***********************************************************************
-// //! name of dish
-// console.log('name of dish: ', data.results[1].name);
-
-// //! image of dish
-// console.log('image: ', data.results[1].beauty_url);
-
-// //! description of the dish
-// console.log('Description: ',data.results[1].description);
-
-// //! Ingredients
-// console.log('ingredients:');
-// console.log(data.results[1].sections[0].components[0].measurements[0].quantity);
-// console.log(data.results[1].sections[0].components[0].measurements[0].unit.abbreviation);
-//console.log(data.results[1].sections[0].components.length);
-// for(let i = 0; i < data.results[1].sections[0].components.length; i++){
-//   console.log(data.results[1].sections[0].components[i].ingredient.name);
-// }
-//console.log(data.results[1].sections[0].components[0].ingredient.name); //***
-//console.log(data.results[1].sections[0].components[0].ingredient.display_singular);
-//console.log(data.results[1].sections[0].components[0].ingredient.display_plural);
-// console.log(data.results[1].sections[0].components[0].extra_comment);
-
-// //! Number of servings
-//console.log(data.results[1].num_servings);
-// console.log(data.results[1].yields);
-
-// //! Step by step instructions
-// console.log('Step ', data.results[1].instructions[0].position);
-// console.log(data.results[1].instructions[0].display_text);
-// console.log('Step ', data.results[1].instructions[1].position);
-// console.log(data.results[1].instructions[1].display_text);
-// console.log('Step ', data.results[1].instructions[2].position);
-// console.log(data.results[1].instructions[2].display_text);
-// console.log('Step ', data.results[1].instructions[3].position);
-// console.log(data.results[1].instructions[3].display_text);
-// console.log('Step ', data.results[1].instructions[4].position);
-// console.log(data.results[1].instructions[4].display_text);
-// console.log('Step ', data.results[1].instructions[5].position);
-// console.log(data.results[1].instructions[5].display_text);
-// console.log('Step ', data.results[1].instructions[6].position);
-// console.log(data.results[1].instructions[6].display_text);
-// console.log('Step ', data.results[1].instructions[7].position);
-// console.log(data.results[1].instructions[7].display_text);
-// console.log('Step ', data.results[1].instructions[8].position);
-// console.log(data.results[1].instructions[8].display_text);
-
-// //! nutrition facts
-// console.log('Calories: ', data.results[1].nutrition.calories);
-// console.log('Carbs: ', data.results[1].nutrition.carbohydrates);
-// console.log('Fiber: ', data.results[1].nutrition.fiber);
-// console.log('Protein: ', data.results[1].nutrition.protein);
-// console.log('Fat: ', data.results[1].nutrition.fat);
-// console.log('Sugar: ', data.results[1].nutrition.sugar);
-
-// //! Video of how to prepare
-// console.log('Video: ', data.results[1].original_video_url);
-
-// //! Difficulty Easy, Medium, Hard
-// console.log('Difficulty: ', data.results[1].topics[0].name);
-
-// //! Meal Time Type Breakfast, Lunch, Dinner
-// console.log('Meal Time: ', data.results[1].topics[3].name);
-
-// //! Cook time
-// console.log('Cook Time: ', data.results[1].total_time_tier
-// .display_tier);
-
-// //! User Rating out of 100%
-// console.log('User Rating: ', data.results[1].user_ratings.score);
